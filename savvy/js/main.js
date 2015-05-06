6f37e20412f5f34bc864db11d9edfddd78dbb40d
Matias Seijas <matiseijas@gmail.com>
2015-05-05 22:14:38 -0400

    Implemented getAttributeRANGE

--- savvy/js/main.js
+++ savvy/js/main.js
@@ -1,5 +1,13 @@
 var gameLog = loadJSON('data/gameLog.json');
 
+/******  SUBCLASSING  ******/
+Array.prototype.max = function() {
+  return Math.max.apply(null, this);
+};
+
+Array.prototype.min = function() {
+  return Math.min.apply(null, this);
+};
 
 /****** DATA LOADING ******/
 
@@ -65,7 +73,7 @@ function getStudentsWithID(studentIDs)
 //   - "level badges"
 //   - "no badges"
 
-function getStudentsForCondition(condition, data)
+function getCondition(condition, data)
 {
   studentData = [];
 
@@ -122,34 +130,128 @@ function getChapters(chapterNumber, data)
 }
 
 
+function getLevel(levelNumber, data)
+{
+  studentData = [];
 
-// function getAttributeSUM(attribute, data)
+  if (data) {
+    for (student in data)
+    {
+      if (data[student].level == levelNumber)
+      {
+        studentData.push(data[student]);
+      }
+    }
+  }
+  else
+  {
+    for (student in gameLog)
+    {
+      if (gameLog[student].level == chapterNumber)
+      {
+        studentData.push(gameLog[student]);
+      }
+    }
+  }
+
+  return studentData;
+}
+
+// function getAttribute(attributeName, data)
 // {
-//   attributeSUM = [];
+//   attributeValues = [];
 
 //   if (data)
 //   {
-//     for (student in data)
-//     {
-//       if (data[student].subjectCond == condition)
-//       {
-//         studentData.push(data[student]);
-//       }
-//     }
+
 //   }
 //   else
 //   {
 //     for (student in gameLog)
 //     {
-//       if (gameLog[student].subjectCond == condition)
-//       {
-//         studentData.push(gameLog[student]);
-//       }
+//       if (gameLog[student].)
 //     }
 //   }
+
+//   return attributeValues;
+// }
+
+function getAttributeRANGE(attributeName, data)
+{
+  attributeRANGE = [];
+
+  if (data)
+  {
+    for (student in data)
+    {
+      attributeRANGE.push((data[student][attributeName]));
+    }
+  }
+  else
+  {
+    for (student in gameLog)
+    {
+      attributeRANGE.push((gameLog[student][attributeName]));
+    }
+  }
+
+  return { 'max' : arrayMax(attributeRANGE), 'min' : arrayMin(attributeRANGE) };
+}
+
+
+// function getAttributeSUM(attributeName, data)
+// {
+//   attributeSUM = [];
+
+//   // if (data)
+//   // {
+//     for (student in gameLog)
+//     {
+//       // if (data[dataAttribute].subjectCond == condition)
+//       // {
+//       //   studentData.push(data[student]);
+//       // }
+//       var row = {};
+//       row['chapter'] = gameLog[student].chapter;
+//       row['level'] = gameLog[student].level;
+//       row[attributeName] = gameLog[student][attributeName];
+
+//       // console.log(gameLog[student].attributeName);
+
+//       attributeSUM.push(row);
+//     }
+//   // }
+//   // else
+//   // {
+//   //   for (student in gameLog)
+//   //   {
+//   //     if (gameLog[student].subjectCond == condition)
+//   //     {
+//   //       studentData.push(gameLog[student]);
+//   //     }
+//   //   }
+//   // }
 
 //   return attributeSUM;
 // }
 
 
+function arrayMin(arr) {
+  var len = arr.length, min = Infinity;
+  while (len--) {
+    if (arr[len] < min) {
+      min = arr[len];
+    }
+  }
+  return min;
+};
 
+function arrayMax(arr) {
+  var len = arr.length, max = -Infinity;
+  while (len--) {
+    if (arr[len] > max) {
+      max = arr[len];
+    }
+  }
+  return max;
+};
