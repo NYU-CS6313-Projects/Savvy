31cf3339dc286bdd421f98bc8f92cd4ef9e84a79
Matias Seijas <matiseijas@gmail.com>
2015-05-05 22:14:38 -0400

    Implemented getAttributeRANGE

--- savvy/js/chapter_charts.js
+++ savvy/js/chapter_charts.js
@@ -1,216 +1,61 @@
 function updateChapterGraphs(chapterAttributeKey, chapterAttributeName)
 {
-    var _dataPath = 'data/ch1Data.csv';
 
-    d3.csv( _dataPath, function ( d ) {
+  // for (var i = 1; i < 5; i++){
+    i = 1;
+    // data = 
+    //   [{"subjectId":3001,"subjectCond":"level badges","chapter":1,"level":1,"playCount":1,"playTime":8,"deaths":0,"badgesEarned":"0","anglesUnlocked":0,"steps":1,"badgeDecision":"NULL"},
+    //    {"subjectId":3001,"subjectCond":"level badges","chapter":1,"level":1,"playCount":1,"playTime":9,"deaths":0,"badgesEarned":"0","anglesUnlocked":0,"steps":2,"badgeDecision":"NULL"},{"subjectId":3001,"subjectCond":"level badges","chapter":1,"level":3,"playCount":1,"playTime":13,"deaths":0,"badgesEarned":"0","anglesUnlocked":0,"steps":5,"badgeDecision":"NULL"},{"subjectId":3001,"subjectCond":"level badges","chapter":1,"level":4,"playCount":1,"playTime":14,"deaths":0,"badgesEarned":"0","anglesUnlocked":1,"steps":3,"badgeDecision":"NULL"}];
+    // data = getChapters(3);
+    // if (){
+      
 
-      var data = d;
-      return data;
+      
 
-    }, function( error, data ) {
+      // data = getStudentsForCondition("no badges", getChapters(3));
+    
+      // var cChart = c3.generate({
+      //       bindto: '.chapter1-chart',
+      //       data: {
+      //         json: data,
+      //         type: 'bar',
+      //         keys: {
+      //             x: 'level',
+      //             value: ['steps']
+      //         }
+      //       },
+      //       axis: {
+      //         y: {
+      //           label: chapterAttributeName,
+      //           min: 0,
+      //           max: 500,
+      //           padding: {top: 0, bottom: 0}
+      //         },
+      //         x: {
+      //           label: {
+      //             text: 'Chapter ' + i,
+      //             position: 'outer-center'
+      //           }
+      //         }
+      //       },
+      //       tooltip: {
+      //         show: true,
+      //       },
+      //       point: {
+      //         show: false
+      //       },
+      //       bar: {
+      //         width: 15
+      //       },
+      //       legend: {
+      //         hide: true,
 
-      if (!error)
-      {
+      //       }
+      // });
+    // }
+  // }
 
-        var chart = c3.generate({
-          bindto: '.chapter1-chart',
-          data: {
-            json: data,
-            type: 'bar',
-            keys: {
-                x: 'Level',
-                value: [chapterAttributeKey]
-            }
-          },
-          axis: {
-            y: {
-              label: chapterAttributeName,
-              max: 550
-            },
-            x: {
-              label: {
-                text: 'Chapter 1',
-                position: 'outer-center'
-              }
-            }
-          },
-          tooltip: {
-            show: true,
-          },
-          point: {
-            show: false
-          },
-          bar: {
-            width: 15
-          },
-          legend: {
-            hide: true,
-
-          }
-        });
-
-      }
-    });
-
-
-
-    var _dataPath = 'data/ch2Data.csv';
-
-    d3.csv( _dataPath, function ( d ) {
-
-      var data = d;
-      return data;
-
-    }, function( error, data ) {
-
-      if (!error)
-      {
-
-        var chart = c3.generate({
-          bindto: '.chapter2-chart',
-          data: {
-            json: data,
-            type: 'bar',
-            keys: {
-                x: 'Level',
-                value: [chapterAttributeKey]
-            }
-          },
-          axis: {
-            y: {
-              label: chapterAttributeName,
-              max: 550
-            },
-            x: {
-              label: {
-                text: 'Chapter 2',
-                position: 'outer-center'
-              }
-            }
-          },
-          tooltip: {
-            show: true
-          },
-          point: {
-            show: false
-          },
-          bar: {
-            width: 15
-          },
-          legend: {
-            hide: true
-          }
-        });
-
-      }
-    });
-
-
-    var _dataPath = 'data/ch3Data.csv';
-
-    d3.csv( _dataPath, function ( d ) {
-
-      var data = d;
-      return data;
-
-    }, function( error, data ) {
-
-      if (!error)
-      {
-
-        var chart = c3.generate({
-          bindto: '.chapter3-chart',
-          data: {
-            json: data,
-            type: 'bar',
-            keys: {
-                x: 'Level',
-                value: [chapterAttributeKey]
-            }
-          },
-          axis: {
-            y: {
-              label: chapterAttributeName,
-              max: 550
-            },
-            x: {
-              label: {
-                text: 'Chapter 3',
-                position: 'outer-center'
-              }
-            }
-          },
-          tooltip: {
-            show: true
-          },
-          point: {
-            show: false
-          },
-          bar: {
-            width: 15
-          },
-          legend: {
-            hide: true
-          }
-        });
-
-      }
-    });
-
-
-      var _dataPath = 'data/ch4Data.csv';
-
-    d3.csv( _dataPath, function ( d ) {
-
-      var data = d;
-      return data;
-
-    }, function( error, data ) {
-
-      if (!error)
-      {
-
-        var chart = c3.generate({
-          bindto: '.chapter4-chart',
-          data: {
-            json: data,
-            type: 'bar',
-            keys: {
-                x: 'Level',
-                value: [chapterAttributeKey]
-            }
-          },
-          axis: {
-            y: {
-              label: chapterAttributeName,
-              max: 500
-            },
-            x: {
-              label: {
-                text: 'Chapter 4',
-                position: 'outer-center'
-              }
-            }
-          },
-          tooltip: {
-            show: true,
-            title: chapterAttributeName
-          },
-          point: {
-            show: false
-          },
-          bar: {
-            width: 15
-          },
-          legend: {
-            hide: true
-          }
-        });
-
-      }
-    });
-};
-
+}
 
 
 function updateChapterAttributes(){
