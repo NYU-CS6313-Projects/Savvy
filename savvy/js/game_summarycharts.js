30fe7354414fee425b294cbe90dd4b92dac489ce
Matias Seijas <matiseijas@gmail.com>
2015-05-07 18:27:46 -0400

    Updated tooltip style

--- savvy/js/game_summarycharts.js
+++ savvy/js/game_summarycharts.js
@@ -41,6 +41,31 @@ function updateGameSummaryCharts()
                 title: function (d) { return 'Level ' + d; },
                 name: function (name, ratio, id, index) { return selectedAttribute.name; },
                 value: d3.format(',')
+            },
+            contents: function (d, defaultTitleFormat, defaultValueFormat, color) {
+                var $$ = this, config = $$.config,
+                    titleFormat = config.tooltip_format_title || defaultTitleFormat,
+                    nameFormat = config.tooltip_format_name || function (name) { return name; },
+                    valueFormat = config.tooltip_format_value || defaultValueFormat,
+                    text, i, title, value, name, bgcolor;
+                for (i = 0; i < d.length; i++) {
+                    if (! (d[i] && (d[i].value || d[i].value === 0))) { continue; }
+
+                    name = nameFormat(d[i].name);
+                    value = valueFormat(d[i].value, d[i].ratio, d[i].id, d[i].index);
+
+                    text = "<table class='" + $$.CLASS.tooltip + "'>";
+
+                    text += "<tr class='" + $$.CLASS.tooltipName + "-" + d[i].id + "' style='border-bottom: none !important; height: 25px !important; padding-bottom: 5px; '>";
+                    text += "<td class='name' style='color: #767676;  font-size: 11px; border-bottom: none !important; padding-top: 8px !important; padding-left: 10px !important;'>" + name + "</td>";
+                    text += "</tr>";
+
+                    text += "<tr class='" + $$.CLASS.tooltipName + "-" + d[i].id + "' style='border-top: none !important; height: 25px !important;'>";
+                    text += "<td class='value' style='text-align: left !important; font-size: 14px !important; padding-left: 10px !important; padding-top: 5px !important; padding-bottom: 8px !important'>" + value + "</td>";
+                    text += "</tr>";
+
+                }
+                return text + "</table>";
             }
           },
           point: {
