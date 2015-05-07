4e7ab9e1b32ab596d0823430c2f5a9bf5cefcb62
Matias Seijas <matiseijas@gmail.com>
2015-05-07 14:09:54 -0400

    Implemented Section 3: Game key metrics

---
+++ savvy/js/game_keymetrics.js
@@ -0,0 +1,19 @@
+function updateGameKeyMetrics(data)
+{
+
+	var playTime = document.getElementById("metric-playtime");
+	var repeats = document.getElementById("metric-repeats");
+	var steps = document.getElementById("metric-steps");
+	var deaths = document.getElementById("metric-deaths");
+	var anglesUnlocked = document.getElementById("metric-anglesunlocked");
+	var badgesEarned = document.getElementById("metric-badgesearned");
+
+
+	playTime.innerHTML = getAttribute('playTime', data).sum;
+	repeats.innerHTML = getAttribute('playCount', data).sum;
+	steps.innerHTML = getAttribute('steps', data).sum;
+	deaths.innerHTML = getAttribute('deaths', data).sum;
+	anglesUnlocked.innerHTML = getAttribute('anglesUnlocked', data).sum;
+	badgesEarned.innerHTML = getAttribute('badgesEarned', data).sum;
+
+}
