edd480c02063fc404ad494c685a44c3121a6ae54
Matias Seijas <matiseijas@gmail.com>
2015-05-07 21:30:47 -0400

    Fixed scatterplot circle opacity

--- savvy/js/scatterplot_chart.js
+++ savvy/js/scatterplot_chart.js
@@ -10,7 +10,19 @@ function updateScatterplot()
       type: 'scatter',
       keys: {
           x: axes.xKey,
-          value: [axes.yKey]
+          value: [axes.yKey], 
+      },
+      color: function (color, d) {
+            // d will be 'id' when called for legends
+            // console.log("new point - " + d.id + " value - " + d.value);
+            console.log(d);
+            console.log("--")
+            gimmeMore(d);
+            console.log(color);
+            // return d3.rgb(color).darker(d.value / 150);
+            var cc = d3.select(d3.selectAll("circle")[0][d.index]).style("fill", "rgba(31, 119, 180, 1)");
+
+            return cc;
       }
     },
     axis: {
@@ -31,16 +43,27 @@ function updateScatterplot()
       show: true
     },
     point: {
-      show: false
+      show: true
     },
     legend: {
       hide: true,
 
-    }
+    },
+    point: {
+      r: 3,
+    },
+     onclick: function(d,i){
+            //console.log(d);
+            alert(d)
+    },
+    
   });
 
   return scatterplotChart;
 
 }
 
-
+function gimmeMore(dataPoint)
+{
+  console.log(studentDemographics[dataPoint.index]);
+}
